c92eafd6b566a4834e59a642673373b7824ae907
x-blood <yasaisato@gmail.com>
2017-12-31 21:56:02 +0900

    Modified main script file

--- handBrake-automate-encoding.sh
+++ handBrake-automate-encoding.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+ENCODED_FILE_COUNT=0
+
 # loop target files
 for file in `\find ${HANDBRAKECLI_INPUT_PATH} -maxdepth 1 -name '*.MOV'`; do
     # get file name
@@ -14,10 +16,15 @@ for file in `\find ${HANDBRAKECLI_INPUT_PATH} -maxdepth 1 -name '*.MOV'`; do
     # move to finished directory
     mv ${INPUT_FILE_PATH_INCLUDE_EXTENSION} ${HANDBRAKECLI_INPUT_PATH}/finished/
 
+    # increment ENCODED_FILE_COUNT
+    ENCODED_FILE_COUNT=$(( ENCODED_FILE_COUNT + 1 ))
 done
 
 # send to Slack
-echo "Start send to Slack"
-echo "Succesful HandBrake-CLI encoding batch" | bash ./sendResultToSlack.sh
-echo "Finished send to Slack"
+if (( ${ENCODED_FILE_COUNT}>0 )) ;
+then
+    echo "Start send to Slack"
+    echo "Succesful HandBrake-CLI encoding batch. Total encoded files counts:${ENCODED_FILE_COUNT}" | bash ./sendResultToSlack.sh
+    echo "Finished send to Slack"
+fi
 

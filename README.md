8b8e14be7eb886fdc5357a47b5e5caf708010a5e
x-blood <yasaisato@gmail.com>
2018-05-11 19:38:55 +0900

    最大処理件数の制御を追加

--- README.md
+++ README.md
@@ -10,7 +10,29 @@ bashを使用しています。よって、Windowsで動作させるにはGit Ba
 
 変換に成功した件数が1件でもある場合はSlackにその旨を通史します。
 
+## 利用する環境変数
+- HANDBRAKECLI_INPUT_PATH
+
+入力ファイルパスを設定する。深い階層構造も見に行く(コード参照)。
+- HANDBRAKECLI_MAX_FILE_COUNT
+
+処理する動画の最大数。<br>
+例えば動画が100とかあった場合にシェルスクリプトが終わらない可能性もあるので、本パラメータで最大処理数を制御する。
+- "HandBrakeCLI"コマンドのパス
+
+環境に応じてパスを設定しておくこと。
+
+- HANDBRAKECLI_WEBHOOKURL
+
+SlackのWeb Hook URLを指定
+
 ## スケジュール設定
 cronで行なっています。<br>
 例）Macの設定例<br>
 0 0-23/3 * * * /bin/bash -l /handBrake-CLI-script/handBrake-automate-encoding.sh
+
+## WindowsでCMDからGitBashを起動しての実行例
+```
+cd "%ProgramFiles%\Git\bin\"
+sh -x D:\tech\xblood-handBrake-CLI-encode-script\handBrake-automate-encoding.sh
+```
--- handBrake-automate-encoding.sh
+++ handBrake-automate-encoding.sh
@@ -3,35 +3,41 @@
 ENCODED_FILE_COUNT=0
 
 # loop target files
-for file in `\find ${HANDBRAKECLI_INPUT_PATH} -maxdepth 3 -name '*.MOV'`; do
-    # get file name
-    INPUT_FILE_PATH_INCLUDE_EXTENSION=${file}
-    echo ${INPUT_FILE_PATH_INCLUDE_EXTENSION}
-    INPUT_FILE_PATH_EXCLUDE_EXTENSION=$(echo ${file} | sed 's/\.[^\.]*$//')
-    echo ${INPUT_FILE_PATH_EXCLUDE_EXTENSION}
+for file in `\find ${HANDBRAKECLI_INPUT_PATH} -maxdepth 3 -name '*.MOV'`;
+do
+  # If it is the maximum processing number, exit the loop
+  if [ ENCODED_FILE_COUNT -ge ${HANDBRAKECLI_MAX_FILE_COUNT} ]; then
+    echo "exit loop"
+    break
+  fi
+  
+  # get file name
+  INPUT_FILE_PATH_INCLUDE_EXTENSION=${file}
+  echo ${INPUT_FILE_PATH_INCLUDE_EXTENSION}
+  INPUT_FILE_PATH_EXCLUDE_EXTENSION=$(echo ${file} | sed 's/\.[^\.]*$//')
+  echo ${INPUT_FILE_PATH_EXCLUDE_EXTENSION}
 
-    # execute encoding by HandBrakeCLI
-    if [ ! -e ${INPUT_FILE_PATH_EXCLUDE_EXTENSION}.mp4 ]; then
-	# if include "rotate" character in file name, add rotate option
-	if [ `echo ${INPUT_FILE_PATH_EXCLUDE_EXTENSION} | grep 'rotate5'` ]; then
-		#It's not working
-		HandBrakeCLI -i ${INPUT_FILE_PATH_INCLUDE_EXTENSION} -o ${INPUT_FILE_PATH_EXCLUDE_EXTENSION}.mp4 --rotate 5
-	else
-		HandBrakeCLI -i ${INPUT_FILE_PATH_INCLUDE_EXTENSION} -o ${INPUT_FILE_PATH_EXCLUDE_EXTENSION}.mp4
-	fi
-	# if you want to move the source to finished directory, remove the comment out.
-	#mv ${INPUT_FILE_PATH_INCLUDE_EXTENSION} ${HANDBRAKECLI_INPUT_PATH}/finished/
-
-	# increment ENCODED_FILE_COUNT
-	ENCODED_FILE_COUNT=$(( ENCODED_FILE_COUNT + 1 ))
+  # execute encoding by HandBrakeCLI
+  if [ ! -e ${INPUT_FILE_PATH_EXCLUDE_EXTENSION}.mp4 ]; then
+    # if include "rotate" character in file name, add rotate option
+    if [ `echo ${INPUT_FILE_PATH_EXCLUDE_EXTENSION} | grep 'rotate5'` ]; then
+      #It's not working
+      HandBrakeCLI -i ${INPUT_FILE_PATH_INCLUDE_EXTENSION} -o ${INPUT_FILE_PATH_EXCLUDE_EXTENSION}.mp4 --rotate 5
+    else
+      HandBrakeCLI -i ${INPUT_FILE_PATH_INCLUDE_EXTENSION} -o ${INPUT_FILE_PATH_EXCLUDE_EXTENSION}.mp4
     fi
+    # if you want to move the source to finished directory, remove the comment out.
+    #mv ${INPUT_FILE_PATH_INCLUDE_EXTENSION} ${HANDBRAKECLI_INPUT_PATH}/finished/
+
+    # increment ENCODED_FILE_COUNT
+    ENCODED_FILE_COUNT=$(( ENCODED_FILE_COUNT + 1 ))
+  fi
 done
 
 # send to Slack
 if (( ${ENCODED_FILE_COUNT}>0 )) ;
 then
-    echo "Start send to Slack"
-    echo "Succesful HandBrake-CLI encoding batch. Total encoded files counts:${ENCODED_FILE_COUNT}" | bash ./sendResultToSlack.sh
-    echo "Finished send to Slack"
+  echo "Start send to Slack"
+  echo "Succesful HandBrake-CLI encoding batch. Total encoded files counts:${ENCODED_FILE_COUNT}" | bash ./sendResultToSlack.sh
+  echo "Finished send to Slack"
 fi
-
